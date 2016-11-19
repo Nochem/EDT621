68a8c6661030d3c739462ed2a080c3a957855f2d
Andreas Mårdén <marden.andreas@gmail.com>
2016-11-19 19:42:35 +0100

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nochem.afm;
 
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.widget.EditText;
 import android.app.Activity;
 import android.net.wifi.WifiManager;
@@ -8,7 +10,7 @@ import android.net.wifi.WifiManager;
 
 public class MainActivity extends Activity {
 
-    private String reminderText, currentNetwork, homeNetwork;
+    private String reminderText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +21,27 @@ public class MainActivity extends Activity {
     EditText input = (EditText) findViewById(R.id.editText);
     WifiManager wifi;
 
+    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
+            .setSmallIcon(R.drawable.notification_icon) //TODO: replace with an actual picture
+            .setContentTitle("Reminder:")
+            .setContentText(input)
+            .setAutoCancel()
+            .setSound(Notification.DEFAULT_SOUND)
+            .setVibrate(Notification.DEFAULT_VIBRATE)
+            .Build();
+
     /* Called by the SAVE button. Starts the program. */
     public void save(){
         reminderText=input.getText().toString();
-        currentNetwork=wifi.getConnectionInfo().getSSID(); //TODO: Fix required permissions in manifes
-        if (wifi.getConnectionInfo().getRssi() == 0) { //Permissions issue here too
-                //reminderAlarm();
+        currentNetwork=wifi.getConnectionInfo().getSSID(); //TODO: Fix required permissions in manifest
+        //Some kind of while case here
+        if (wifi.getConnectionInfo().getRssi() == 0) {
+             mBuilder.notify();
         }
     }
 }
 
-/*
+/*      String currentNetwork, homeNetwork;
         homeNetwork=input2.getText().toString();  //Suggested code to make sure it won't go off on all wifis
         if (currentNetwork.equals(homeNetwork)) {
 */
